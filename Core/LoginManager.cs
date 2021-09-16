73f50b3b659df2c33baaa57e068405f7f7257d77
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2021-09-16 17:26:58 +0200

    [RED] - AddUserNameAndPasswordRegistrationTest

--- Core/LoginManager.cs
+++ Core/LoginManager.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Core
 {
+    
+
+
     public class LoginManager
     {
+        public string userName;
+        public string userPassword;
+
+        public void AddNewUser(string s1, string s2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Tests/LoginTestSuite.cs
+++ Tests/LoginTestSuite.cs
@@ -6,10 +6,26 @@ namespace Tests
 {
     public class LoginTestSuite
     {
+        
         [Fact]
-        public void UsernameAndPasswordRegistrationTest()
+        public void AddUserNameAndPasswordRegistrationTest()
         {
+            //ARRANGE
             LoginManager loginManager = new LoginManager();
+
+            //ACT
+            
+            loginManager.AddNewUser("Default_User", "Default_Password");
+            string user = loginManager.userName;
+            string password = loginManager.userPassword;
+
+            //ASSERT
+
+            Assert.Equal("Default_User", user );
+
+            Assert.Equal("Default_Password", password );
+
+
         }
     }
 }
